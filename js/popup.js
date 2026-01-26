5690852d4a6e372f7a4b22b4f194cd8c16f5f9b2
slurry <matvei0972@gmail.com>
2026-01-27 00:41:14 +0300

    popup added 2.0

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,8 @@
-const el = document.querySelector(".ecEhnF");
-if (el) {
-  el.style.backgroundColor = "#222";
+myFunction(){
+    var checkBox = document.getElementById("myCheck");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
 }
