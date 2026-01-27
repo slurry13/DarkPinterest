c090bc500e323cd57da161cc22f6bfee77d3cde3
slurry <matvei0972@gmail.com>
2026-01-27 20:37:42 +0300

    test js

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,15 @@
-myFunction(){
-    var checkBox = document.getElementById("myCheck");
-    if (checkBox.checked == true){
-        text.style.display = "block";
+const fs = require('fs');
+
+const data = fs.readFileSync('../manifest.json', {encoding : 'utf-8'});
+let dataObj = JSON.parse(data);
+
+
+document.getElementById('myCheckBox').addEventListener('click', function () {
+    var checked = this.checked;
+
+    if (checked) {
+        alert("hh");
     } else {
-        text.style.display = "none";
+        alert("gg");
     }
-}
+});​
